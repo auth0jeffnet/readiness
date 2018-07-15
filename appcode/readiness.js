2fa1120b676602a86a879aa11f0649fc33fb0361
Jeff Jeffries <auth0@jeffnet.net>
2018-07-15 16:11:50 -0500

    readiness: updated readiness to utilize bunyan logging

--- appcode/readiness.js
+++ appcode/readiness.js
@@ -1,11 +1,30 @@
-http=require('http')
+// declare variables for the require objects
+var http = require('http');
+var bunyan = require('bunyan');
 
+// configuration: TODO: should go into a configuration file
 iPortNumber = 54321;
+sLogFileErrorLog = '/tmp/readiness-error.log'
+
+var log = bunyan.createLogger({
+    name: "readiness",
+    level: 0,
+    streams: [
+      {
+        level: 'info',
+        path: sLogFileErrorLog
+      },
+      {
+        level: 'error',
+        stream: process.stdout
+      }
+    ]
+});
 
 http.createServer(function (req, res) {
    res.writeHead(200, {'Content-Type': 'text/plain'});
    res.end('readiness is not yet ready\n');
 }).listen(iPortNumber);
 
-console.log('readiness server running at port %d', iPortNumber);
+log.info('readiness server running at port %d', iPortNumber);
 
