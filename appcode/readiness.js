e4ba85ba6fb2b3e322d0e1f421e1081bfc698019
Jeff Jeffries <auth0@jeffnet.net>
2018-07-15 16:19:51 -0500

    readiness: updated readiness with configuration variables and comments

--- appcode/readiness.js
+++ appcode/readiness.js
@@ -2,29 +2,39 @@
 var http = require('http');
 var bunyan = require('bunyan');
 
+////////////////////////////////////////////////////////////////
 // configuration: TODO: should go into a configuration file
+////////////////////////////////////////////////////////////////
+// the port number for the report
 iPortNumber = 54321;
-sLogFileErrorLog = '/tmp/readiness-error.log'
+// the log file for the service
+sLogFileInfoLog = '/tmp/readiness-info.log'
 
+// declare a bunyan logging instance
 var log = bunyan.createLogger({
     name: "readiness",
     level: 0,
     streams: [
       {
         level: 'info',
-        path: sLogFileErrorLog
+        path: sLogFileInfoLog
       },
       {
         level: 'error',
         stream: process.stdout
       }
     ]
-});
+}); 
 
+// create the server handler for the report
 http.createServer(function (req, res) {
    res.writeHead(200, {'Content-Type': 'text/plain'});
    res.end('readiness is not yet ready\n');
 }).listen(iPortNumber);
 
 log.info('readiness server running at port %d', iPortNumber);
+
+// TODO: register a callback to handle shutdown properly
+
+// TODO: perform the application loop
 
