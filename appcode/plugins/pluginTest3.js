212f54ac8504e093a48bdbc6812b2c9c5719f3f3
Jeff Jeffries <auth0@jeffnet.net>
2018-07-18 07:43:33 +0000

    readiness: updated the plugins to expose both the short description and long description attributes

--- appcode/plugins/pluginTest3.js
+++ appcode/plugins/pluginTest3.js
@@ -2,6 +2,8 @@
 iFrequencyInSeconds = 15;
 sVersionPlugin = "20180718r001";
 sNamePlugin = "Test Plugin 3 at 15 seconds";
+sDescriptionPluginShort = "Performs a test every 15 seconds";
+sDescriptionPluginLong = "Performs a TODO SOME KIND OF TEST test every 15 seconds (four times per minute)";
 
 module.exports = {
 
@@ -10,6 +12,12 @@ module.exports = {
    },
    getPluginVersion: function(log) {
       return sVersionPlugin;
+   },
+   getPluginDescriptionShort: function(log) {
+      return sDescriptionPluginShort;
+   },
+   getPluginDescriptionLong: function(log) {
+      return sDescriptionPluginLong;
    },
    getPluginName: function(log) {
       return sNamePlugin;
--- appcode/plugins/pluginTest4.js
+++ appcode/plugins/pluginTest4.js
@@ -2,6 +2,8 @@
 iFrequencyInSeconds = 0;
 sVersionPlugin = "20180718r001";
 sNamePlugin = "Test Plugin 4 at 0 seconds (check once only)";
+sDescriptionPluginShort = "Performs a test only once";
+sDescriptionPluginLong = "Performs a TODO SOME KIND OF TEST only once at readiness service startup";
 
 module.exports = {
 
@@ -10,6 +12,12 @@ module.exports = {
    },
    getPluginVersion: function(log) {
       return sVersionPlugin;
+   },
+   getPluginDescriptionShort: function(log) {
+      return sDescriptionPluginShort;
+   },
+   getPluginDescriptionLong: function(log) {
+      return sDescriptionPluginLong;
    },
    getPluginName: function(log) {
       return sNamePlugin;
