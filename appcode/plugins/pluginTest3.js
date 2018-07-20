ef5f49c66d1f868ce5978662feb46af7730c0546
Jeff Jeffries <auth0@jeffnet.net>
2018-07-20 02:14:58 +0000

    readiness: updated the third plugin stub to perform a CURL request for connectivity checks

--- appcode/plugins/pluginTest3.js
+++ appcode/plugins/pluginTest3.js
@@ -1,16 +1,64 @@
 
+var child_process = require('child_process');
+
+sCurlRequest = '/usr/bin/curl --max-time 10 --stderr - http://apt-mirror.it.auth0.com';
+
 module.exports = {
 
    frequency_in_seconds: 15,
    version: "20180718r001",
-   name: "Test Plugin 3 at 15 seconds",
-   summary: "Performs a test every 15 seconds",
-   description: "Performs a TODO SOME KIND OF TEST test every 15 seconds (four times per minute)",
+   name: "curl_check_updates_http",
+   summary: "Performs a curl connectivity test against the URL http://apt-mirror.it.auth0.com",
+   description: "Performs a curl connectivity test against the URL http://apt-mirror.it.auth0.com with a timeout of 10 seconds",
+   helplink: "https://auth0.com/docs/appliance/infrastructure/ip-domain-port-list",
 
    runPlugin: function(log,fxResultsCallback) {
-      sReturnData = '{"name": "' + this.name + '"}';
+      sPluginData = this.getData(log);
+      sReturnData = this.renderPluginData(log,sPluginData);
       fxResultsCallback(log,sReturnData);
-   }
+   },
 
+   getData: function(log) {
+      sOutput = "";
+      try {
+         sOutput = child_process.execSync(sCurlRequest, (err, stdout, stderr) => {
+            sOutput += stdout;
+            sOutput += stderr;
+         });
+      } catch (ex) {
+         sOutput += ex.stdout;
+         sOutput += ex.stderr;
+      };
+      return sOutput;
+   },
+
+   renderPluginData: function(log,sPluginData) {
+      sData = '{';
+
+      sData += '"name"';
+      sData += ': ';
+      sData += '"';
+      sData += this.name;
+      sData += '"';
+
+      sData += ',';
+
+      sData += '"helplink"';
+      sData += ': ';
+      sData += '"';
+      sData += this.helplink;
+      sData += '"';
+
+      sData += ',';
+
+      sData += '"results"';
+      sData += ': ';
+      sData += JSON.stringify( sPluginData );
+
+      sData += '}';
+
+      return sData;
+   }
 };
+
 
